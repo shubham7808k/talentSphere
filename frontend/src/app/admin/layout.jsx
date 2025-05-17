e4bbd02aeb01cfbf3a4637dd81dd532e0c14d029
0802sonali <sonaliprajapati0562@gmailcom>
2025-05-17 11:11:15 +0530

    admin dashboard

--- frontend/src/app/admin/layout.jsx
+++ frontend/src/app/admin/layout.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from './Navbar';
+
 
 const Layout = ({ children }) => {
     return (
         <>
-            <Navbar />
+          
             {children}
         </>
     )
